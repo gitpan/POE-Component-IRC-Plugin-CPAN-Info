780e47fab8cc7e31594ee39216425de10ac8e1c0
Zoffix Znet <zoffixcpan@gmail.com>
2008-02-07 00:47:17 +0000

    Import of ZOFFIX/POE-Component-IRC-Plugin-CPAN-Info-0.02 from CPAN.
    gitpan-cpan-distribution: POE-Component-IRC-Plugin-CPAN-Info
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ZOFFIX/POE-Component-IRC-Plugin-CPAN-Info-0.02.tar.gz
    gitpan-cpan-author:       ZOFFIX
    gitpan-cpan-maturity:     released

--- examples/info_bot.pl
+++ examples/info_bot.pl
@@ -5,13 +5,12 @@ use warnings;
 
 use lib '../lib';
 
-sub POE::Kernel::ASSERT_DEFAULT () { 1 }
 use POE qw(Component::IRC Component::IRC::Plugin::CPAN::Info);
 
 my @Channels = ( '#zofbot' );
 
 my $irc = POE::Component::IRC->spawn( 
-        nick    => 'CPAN2_',
+        nick    => 'CPANInfoBot',
         server  => 'irc.freenode.net',
         port    => 6667,
         ircname => 'CPAN module information bot',
@@ -23,10 +22,6 @@ POE::Session->create(
             qw(
                 _start
                 irc_001
-                _default
-                cpaninfo_got_info
-                cpaninfo_no_result
-                cpaninfo_response
             )
         ],
     ],
@@ -41,8 +36,7 @@ sub _start {
     # register our plugin
     $irc->plugin_add(
         'CPANInfo' => 
-            POE::Component::IRC::Plugin::CPAN::Info->new( debug => 1,
-            max_modules_limit => 1000)
+            POE::Component::IRC::Plugin::CPAN::Info->new
     );
     
     $irc->yield( connect => { } );
@@ -53,35 +47,6 @@ sub irc_001 {
     my ( $kernel, $sender ) = @_[ KERNEL, SENDER ];
     $kernel->post( $sender => join => $_ )
         for @Channels;
-    $kernel->post( $sender => privmsg => 'NickServ' => 'identify se3P4nG0dn4ss!!!');
     undef;
 }
 
-sub cpaninfo_got_info {
-    print "\nGot CPAN info at " . localtime $_[ARG0] . "\n";
-}
-use Data::Dumper;
-sub cpaninfo_no_result {
-    print Dumper($_[ARG0]);
-}
-sub cpaninfo_response {
-    print Dumper($_[ARG0]);
-}
-
-sub _default {
-    my ($event, $args) = @_[ARG0 .. $#_];
-    return
-        if $event =~ /^irc_(332|333|37[26]|25[124])/;
-        
-    my @output = ( "$event: " );
-
-    foreach my $arg ( @$args ) {
-        if ( ref($arg) eq 'ARRAY' ) {
-                push( @output, "[" . join(" ,", @$arg ) . "]" );
-        } else {
-                push ( @output, "'$arg'" );
-        }
-    }
-    print STDOUT join ' ', @output, "\n";
-    return 0;
-}
--- lib/POE/Component/IRC/Plugin/CPAN/Info.pm
+++ lib/POE/Component/IRC/Plugin/CPAN/Info.pm
@@ -3,7 +3,7 @@ package POE::Component::IRC::Plugin::CPAN::Info;
 use warnings;
 use strict;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Carp;
 use POE;
